5cd52ba26328d249b40b6c2c2f80e047b3d9f25c
suriya <suriyaganesh097@gmail.com>
2024-03-17 12:53:20 -0400

    Adding secure aggregation base

--- src/server/main.py
+++ src/server/main.py
@@ -6,15 +6,54 @@ from cryptography.hazmat.primitives.asymmetric import rsa, padding
 app = Flask(__name__)
 
 
-@app.route('/process-data', methods=['POST'])
-def process_data():
-    if request.method == 'POST':
-        part1_bytes = request.files['part1'].read()
-        part2_bytes = request.files['part2'].read()
+class Leader():
+    def __init__(self):
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
+        self.public_key = self.private_key.public_key()
+        self.data_list = []
+
+    def get_public_key(self):
+        return self.public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+    def add(self, data):
+        return self.data_list.append(data)
+
+    def decrypt(self, data):
+        self.data_list.append(self.private_key.decrypt(
+            data,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        )
+        return 'Data processed successfully!'
 
 
-        decrypted_data = private_key.decrypt(
-            part1_bytes,
+class Helper():
+    def __init__(self):
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
+        self.public_key = self.private_key.public_key()
+
+    def get_public_key(self):
+        return self.public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+    def decrypt(self, data):
+        return self.private_key.decrypt(
+            data,
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA256()),
                 algorithm=hashes.SHA256(),
@@ -22,30 +61,46 @@ def process_data():
             )
         )
 
-        part1_data = io.BytesIO(part1_bytes)
-        part2_data = io.BytesIO(part2_bytes)
 
+leader = Leader()
+helper = Helper()
+
+
+@app.route('/process-data', methods=['POST'])
+def process_data():
+    if request.method == 'POST':
+        part1_bytes = request.files['part1'].read()
+        part2_bytes = request.files['part2'].read()
+
+        part1_decrypted_data = leader.decrypt(
+            part1_bytes
+        )
+
+        part2_decrypted_data = helper.decrypt(
+            part2_bytes
+        )
 
         return 'Data processed successfully!'
     else:
         return 'Only POST requests are supported.'
 
 
-# Generate a private key (Do this once and store securely)
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048
-)
-public_key = private_key.public_key()
-
-
 @app.route('/get-public-key')
 def get_public_key():
-    pem = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-    return jsonify({'public_key': pem.decode('utf-8')})
+    resp = {
+        "pk1": {'public_key': leader.get_public_key().decode('utf-8')},
+        "pk2": {'public_key': helper.get_public_key().decode('utf-8')}
+    }
+
+    return jsonify(resp)
+
+
+@app.route('/admin/aggregate')
+def aggregate():
+    resp = {
+        "data": leader.data_list
+    }
+    return jsonify(resp)
 
 
 if __name__ == '__main__':
